298d9bf9fb0cccec117216307645eed6489209d5
Thomas <thom_car@yahoo.fr>
2021-04-26 17:48:42 +0200

    :construction: update profile
    - update front
    - add profile.js for menus

--- themes/pie/header.php
+++ themes/pie/header.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/dc883addae.js" crossorigin="anonymous"></script>
 
 	<?php wp_head(); ?>
