12dff4fc6cb5bc6abcea09b1242f415542c85aee
louis-axel <louisaxelrenard@gmail.com>
2021-04-26 17:41:54 +0200

    partie 1 page posts

--- themes/pie/page-posts.php
+++ themes/pie/page-posts.php
@@ -1,5 +1,4 @@
-
-    <?php
+<?php
 /**
  * The template for displaying all pages
  *
@@ -16,36 +15,69 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
+    <?php
         if ( have_posts()) : ?>
 
 
 
-<div class="row">
-<?php
+    <div class="row">
+        <?php
 		while ( have_posts() ) :
-            the_post()	;		
+            the_post()	;
+            		
 		?>
-        <div>
-        
-        <?php  foreach(get_posts() as $post) : 	?>
-         <h2 class="titre_posts"> <?= $post->post_title	?> </h2> 
-         <p class="description_posts">  <?= get_field('description')?> </p>    
-         <img class="img_posts" src="<?=  get_field('photo')?>"></img> 
-         <?php  endforeach; ?> 
-        
 
-          
+        <?php  $i=0; foreach(get_posts() as $post) : 	?>
+            
+        <div class="row">
+        <?php   if($i<5) :	?>
+            <?php if($i==0): ?>
+            <div class="col-lg-6">
+               
+                <img class="img_posts" src="<?=  get_field('photo')?>"></img> 
+            </div>
+            <?php endif ?>
+            <div class="col-lg-6">
+                <div class="row">
+                 <?php if($i==1): ?>
+                    <div class="col-lg-6">
+                        <div class="row">
+                        <img class="img_posts" src="<?=  get_field('photo')?>"></img> <?php endif ?>
+                        </div>
+                        <?php if($i==2): ?>
+                            <div class="row">
+                            <img class="img_posts" src="<?=  get_field('photo')?>"></img> <?php endif ?>
+                    </div>
+                            </div>
+                        
+                    <div class="col-lg-6">
+                        <?php if($i==3): ?>
+                        <img class="img_posts" src="<?=  get_field('photo')?>"></img> <?php endif ?>
+                        <?php if($i==4): ?>
+                        <img class="img_posts" src="<?=  get_field('photo')?>"></img> <?php endif ?>
+                    </div>
+                </div>
+
+
+            </div>
+            <!-- <h2 class="titre_posts"> <?= $post->post_title	?> </h2> 
+         <p class="description_posts">  <?= get_field('description')?> </p> -->
+
+
+            <?php $i++; endif;  endforeach; ?>
+
+
+
 
         </div>
-</div>
- <?php   endwhile; // End of the loop. 
+    </div>
+    <?php  endwhile; // End of the loop. 
  endif;
  ?>
- 
-	</main><!-- #main -->
+
+</main><!-- #main -->
 
 <?php
 /*get_sidebar();*/
