0ef8b11cccbe931f5c0f1e2e5009f8c2c1ab5353
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2021-04-30 17:24:27 +0100

    tool bar seulement pour admin

--- themes/pie/functions.php
+++ themes/pie/functions.php
@@ -188,3 +188,7 @@ function wpm_admin_redirection() {
 	}
 }
 add_action( 'init', 'wpm_admin_redirection' );
+
+if (!current_user_can( 'manage_options' )) {
+	add_filter('show_admin_bar', '__return_false');
+}
